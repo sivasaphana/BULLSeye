0395f9f17f08ec18de80a1b2a6e2df932dc2b856
KarthicJ <KarthicJ@users.noreply.github.com>
2015-10-28 23:59:58 -0700

    Update wfa_ver_1.r
    add libraries

--- wfa_ver_1.r
+++ wfa_ver_1.r
@@ -12,10 +12,9 @@ data_po <- read.csv("C:/PROJECT/data_po/data_po.csv")
 #data_po$time2 <- strptime(data_po$Complete.Timestamp, "%Y/%m/%d %H:%M:%OS")
 data_po$Timediff <- as.numeric(difftime(data_po$Complete.Timestamp,data_po$Start.Timestamp,units="hours"))
 View(data_po)
-
+#
 df<- sel
-
+#
 Rolebased <- sqldf("select Role,sum(Timediff) as Cumulative_Timediff,count(Role) as Frequency from data_po group by Role")
-
 View(Rolebased)
 
