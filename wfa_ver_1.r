3c16b3a864f0a942f97ff5f09d77f9acf4253b48
KarthicJ <KarthicJ@users.noreply.github.com>
2015-10-30 06:55:05 -0700

    Update wfa_ver_1.r

--- wfa_ver_1.r
+++ wfa_ver_1.r
@@ -7,14 +7,16 @@ library(plyr)
 library(tcltk2)
 
 #work flow analysis - first look
-data_po <- read.csv("C:/PROJECT/data_po/data_po.csv")
+ProjectDir <- getwd()
+CodeDir <- file.path(ProjectDir, "R")
+data_po <- read.csv(file.path(ProjectDir, "data_po.csv")
 #data_po$time1 <- strptime(data_po$Start.Timestamp, "%Y/%m/%d %H:%M:%OS")
 #data_po$time2 <- strptime(data_po$Complete.Timestamp, "%Y/%m/%d %H:%M:%OS")
 data_po$Timediff <- as.numeric(difftime(data_po$Complete.Timestamp,data_po$Start.Timestamp,units="hours"))
-View(data_po)
+#View(data_po)
 #
 df<- sel
 #
 Rolebased <- sqldf("select Role,sum(Timediff) as Cumulative_Timediff,count(Role) as Frequency from data_po group by Role")
-View(Rolebased)
+#View(Rolebased)
 
